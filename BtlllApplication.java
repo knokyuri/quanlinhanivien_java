2b8df22e26198552933c342b79420030ad7cef6a
knokyuri <trinhcongthanh2707@gmail.com>
2026-04-06 16:30:44 +0700

    Add files via upload

---
+++ BtlllApplication.java
@@ -0,0 +1,13 @@
+package com.example.btlll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtlllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtlllApplication.class, args);
+	}
+
+}
